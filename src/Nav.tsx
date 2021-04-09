9d51584af4b4b1aa43541b878d00ad47e0c30ae8
Farasat Ali <farasataliazeemi1@gmail.com>
2021-04-09 23:59:53 +0500

    Created and Updated Nav Component

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import "./Nav.css";
 
 const Navbar = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    // return () => {
+    //   window.removeEventListener("scroll");
+    // };
+  }, []);
+
   return (
-    <div className="nav">
-      <img className="nav__logo" src="" alt="Netflix Logo" />
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://pbs.twimg.com/profile_images/124011999041155"
+        alt="Netflix Logo"
+      />
     </div>
   );
 };
