b593e239c049a62dea22848882356e014b64f2c2
Farasat Ali <farasataliazeemi1@gmail.com>
2021-04-10 00:37:18 +0500

    Finalized the App

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,4 +1,6 @@
 import { FC, useEffect, useState } from "react";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import "./Row.css";
 import instance from "./axios";
 
@@ -10,6 +12,7 @@ interface IRow {
 
 const Row: FC<IRow> = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState<any>([]);
+  const [trailerUrl, setTrailerUrl] = useState<string | null>("");
   const base_url = "https://image.tmdb.org/t/p/original/";
 
   useEffect(() => {
@@ -21,6 +24,30 @@ const Row: FC<IRow> = ({ title, fetchUrl, isLargeRow }) => {
     fetchData();
   }, [fetchUrl]);
 
+  const opts: any = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie: any) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url: any) => {
+          // https://youtube.com/watch?v=dasasdasadadwq&banana=5  below work is to get value of v not banana
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error: any) => {
+          console.log(error);
+        });
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -29,6 +56,7 @@ const Row: FC<IRow> = ({ title, fetchUrl, isLargeRow }) => {
           return (
             <img
               key={movie.id}
+              onClick={() => handleClick(movie)}
               className={`row__poster  ${isLargeRow && "row__posterLarge"}`}
               src={`${base_url}${
                 isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -38,6 +66,7 @@ const Row: FC<IRow> = ({ title, fetchUrl, isLargeRow }) => {
           );
         })}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
